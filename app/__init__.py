ccbec838072f2422c340b11add14eece4b6fd580
luminocity <144556348+kunigitaj@users.noreply.github.com>
2024-08-25 14:27:56 -0400

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -3,23 +3,32 @@
 from flask import Flask
 from flask_caching import Cache
 from .logger import setup_logger
+import logging
 
 cache = Cache()  # Create the cache instance
 
 def create_app():
-    app = Flask(__name__)
+    logging.info("Initializing the Flask application...")
 
+    app = Flask(__name__)
+    
     # Load configurations
     app.config.from_object('config.Config')
+    logging.info("Configurations loaded.")
 
     # Setup logging
     setup_logger(app)
+    logging.info("Logger setup completed.")
 
     # Initialize caching with the app
     cache.init_app(app)  # Attach the cache to the app
+    logging.info("Cache initialized.")
 
     # Register routes
     with app.app_context():
         from . import routes
+        logging.info("Routes registered.")
 
+    logging.info("Flask application initialized successfully.")
+    
     return app
--- run.py
+++ run.py
@@ -3,12 +3,24 @@
 from app import create_app
 from dotenv import load_dotenv
 import os
+import logging
 
 # Load environment variables from .env file
 load_dotenv()
 
+# Setup logging format
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
+
 app = create_app()
 
 if __name__ == '__main__':
+    logging.info("Starting the Flask application...")
+
     # For local development
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=bool(os.getenv("FLASK_DEBUG", False)))
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    logging.info(f"Debug mode is {'on' if debug_mode else 'off'}")
+
+    port = int(os.environ.get('PORT', 5001))
+    logging.info(f"Running on http://0.0.0.0:{port}")
+
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
