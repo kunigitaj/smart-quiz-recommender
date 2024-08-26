6d5f80010d0d84100dc274c6668d3bbaae50b76d
luminocity <144556348+kunigitaj@users.noreply.github.com>
2024-08-26 07:23:43 -0400

    refactored

--- app/custom_tokenize.py
+++ app/custom_tokenize.py
@@ -1,13 +1,14 @@
 # app/custom_tokenize.py
 
 import warnings
-from transformers import AutoTokenizer
+from sentence_transformers import SentenceTransformer
 
 # Suppress specific warning
 warnings.filterwarnings("ignore", message=r".*clean_up_tokenization_spaces.*", category=FutureWarning)
 
 def custom_tokenize(text, model_name='sentence-transformers/all-mpnet-base-v2'):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = SentenceTransformer(model_name)
+    tokenizer = model.tokenizer
     
     # Manually clean up tokenization spaces
     cleaned_text = text.replace("  ", " ").strip()
